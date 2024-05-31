9306bbc0df94c3d7a5e414db512e079e16e92091
Saba Din <138373808+Saba-fct@users.noreply.github.com>
2024-05-31 14:46:42 +0100

    Add files via upload

---
+++ Project5_RenamingFilesFromDirectory.py
@@ -0,0 +1,7 @@
+import os
+path = '/Users/PRE-SETUP/Documents/Python Projects'
+files = os.listdir(path)
+
+
+for index, file in enumerate(files):
+    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), '.py'])))
